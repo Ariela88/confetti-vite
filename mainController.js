dd8a5d29a364e20c6f086214116d26756660675d
Ariela88 <135212709+Ariela88@users.noreply.github.com>
2023-07-24 13:15:00 +0200

    first commit

--- mainController.js
+++ mainController.js
@@ -1,5 +1,5 @@
 
-import { confetti } from "canvas-confetti";
+import confetti from "canvas-confetti"
 
 
 export class MainController {
@@ -21,13 +21,16 @@ export class MainController {
         button2.appendChild(document.createTextNode('Fire 2'))
         button3.appendChild(document.createTextNode('Fire 3'))
 
-        button1.addEventListener('click',()=>{confetti({
-            particleCount: 100,
-            spread: 70,
-            origin: { y: 0.6 }
-          });
-
+        button1.addEventListener('click',()=>{
+            confetti({
+                particleCount: 100,
+                spread: 70,
+                origin: { y: 0.6 }
+              });
         })
+
+   
+      
 
         mainContainer.appendChild(button1)
         mainContainer.appendChild(button2)
